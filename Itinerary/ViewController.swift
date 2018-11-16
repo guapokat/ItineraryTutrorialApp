942348a15781ef93f178785a2584ab76733ad927
Virgil Martinez <virgilmartinez@gmail.com>
2018-11-15 20:32:16 -0600

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Virgil Martinez on 11/15/18.
+//  Copyright © 2018 Vidzu Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
