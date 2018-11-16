0d9505a4df9b02de571afb448b4c7c10e3f6b4fa
Virgil Martinez <virgilmartinez@gmail.com>
2018-11-15 23:08:33 -0600

    Added readTrips code.

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Virgil Martinez on 11/15/18.
+//  Copyright © 2018 Vidzu Apps. All rights reserved.
+//
+
+import Foundation
+
+//creates mock data and stores
+
+class Data {
+    static var tripModels = [TripModel]()
+}
